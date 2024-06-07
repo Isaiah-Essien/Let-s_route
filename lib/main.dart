ed3aa76fe49e3842fb19e712293f34a19db1e307
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-06-07 14:55:57 +0200

    practice routr assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:lets_route/pages/first_page.dart';
 import 'package:lets_route/pages/home_page.dart';
+import 'package:lets_route/pages/details_page.dart'; // Import the details page
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -37,10 +38,12 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const FirstPage(),
+      initialRoute: '/firstpage',
       routes: {
         '/firstpage': (context) => const FirstPage(),
         '/homepage': (context) => const HomePage(),
+        '/details': (context) =>
+            const DetailsPage(), // Add the details page route
       },
     );
   }
